cff928761217f03468f19fa0807922186c12b401
marcosvile <marcosvilelateixeira.com>
2021-11-06 16:04:24 -0300

    ok

---
+++ Algoritmos/# Calculo de desconto.py
@@ -0,0 +1,5 @@
+# Calculo de desconto
+# Algoritmo que pretende calcular o desconto sobre o valor de compra de mercadorias, seguindo o critério:
+# Se  o cliente comprar entre R$500 e R$1000 ele ganha um desconto de 5% sobre o valor de compra.
+# Caso o cliente compre mais do que R$1000 ele recebe um desconto de 10%.
+# O algoritmo informa, o valor da compra com desconto, e troco.
