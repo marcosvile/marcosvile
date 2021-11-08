86e27cf375f434234504672a1d295f75889d51a5
marcosvile <marcosvilelateixeira.com>
2021-11-08 20:50:25 -0300

    ok

--- POO básico SOLYD/Main.py
+++ POO básico SOLYD/Main.py
@@ -1,5 +1,6 @@
 # Aula 09 - POO
 # Main - onde meu programa principal irá rodar
 
+from veiculo import Veiculo
 
-
+caminhao_rosa = Veiculo('rosa', 'ford', 6)
---
+++ POO básico SOLYD/veiculo.py
@@ -0,0 +1,8 @@
+class Veiculo:
+    
+    def __init__(self, cor, rodas, marca):
+        self.cor = cor
+        self.rodas = rodas
+        self.marca = marca
+
+# nunca se executa a classe, ela é apenas uma ferramenta para a main
