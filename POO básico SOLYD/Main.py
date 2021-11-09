9f0127028e39bfe664b85c443e4b75113258b4e1
marcosvile <marcosvilelateixeira.com>
2021-11-08 21:08:44 -0300

    ok

--- POO básico SOLYD/Main.py
+++ POO básico SOLYD/Main.py
@@ -1,6 +1,29 @@
 # Aula 09 - POO
 # Main - onde meu programa principal irá rodar
 
+from typing import ForwardRef
 from veiculo import Veiculo
 
-caminhao_rosa = Veiculo('rosa', 'ford', 6)
+caminhao_rosa = Veiculo('rosa', 6, 'ford', 10)
+
+print(caminhao_rosa)
+print(type(caminhao_rosa))
+
+print("cor: ", caminhao_rosa.cor)
+print("Marca: ", caminhao_rosa.marca)
+print("Tanque: ", caminhao_rosa.tanque)
+
+
+carro_azul = Veiculo("azul", 5, "BMW", 17)
+
+print("#######################")
+print("CARRO AZUL: ")
+print("Cor: ", carro_azul.cor )
+print("Rodas: ", carro_azul.rodas)
+print("Marca: ", carro_azul.marca)
+print("Tanque: ", carro_azul.tanque)
+
+carro_azul.abastecer(35)
+print("Tanque: ", carro_azul.tanque)
+
+
--- POO básico SOLYD/veiculo.py
+++ POO básico SOLYD/veiculo.py
@@ -1,8 +1,12 @@
 class Veiculo:
     
-    def __init__(self, cor, rodas, marca):
+    def __init__(self, cor, rodas, marca, tanque):
         self.cor = cor
         self.rodas = rodas
         self.marca = marca
+        self.tanque = tanque
+
+    def abastecer(self, litros):
+        self.tanque += litros
 
 # nunca se executa a classe, ela é apenas uma ferramenta para a main
