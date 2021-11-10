4708bc4ed423b661a6a1b2f08e062aa02b3e6224
marcosvile <marcosvilelateixeira.com>
2021-11-09 21:44:39 -0300

    correcao de arquivos e pastas

---
+++ Banco Contoso/# Exercicio Aula 09.py
@@ -0,0 +1,15 @@
+# Exercicio Aula 09
+# Crie um software de gerenciamento bancario
+# esse software devera ser capaz de criar clientes e contas
+# cada cliente possui:
+# nome 
+# cpf 
+# idade
+
+# cada conta possui:
+# cliente - cliente dono da conta
+# saldo
+# limite
+# sacar
+# depositar 
+# e consultar saldo 
